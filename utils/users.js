4843cd997b6b603a6414fff755e33955fbe23ce6
ElderNobrega <eldernguerra.filho@hotmail.com>
2020-03-08 05:21:47 -0400

    User classes in ES6

---
+++ utils/users.js
@@ -0,0 +1,40 @@
+/*[{
+    id: 'adfadf',
+    name: 'nameee',
+    room: 'node room'
+}]
+*/
+
+class Users {
+    constructor() {
+        this.users = [];
+    }
+
+    addUser(id, name, room) {
+        let user = {id, name, room};
+        this.users.push(user)
+        return user;
+    }
+
+    getUserList(room) {
+        let users = this.users.filter((user) => user.room === room);
+        let namesArr = users.map((user) => user.name);  
+        return namesArr;
+    }
+
+    getUser(id) {
+        return this.users.filter((user) => user.id === id)[0];
+    }
+
+    removeUser(id) {
+        let user = this.getUser(id);
+
+        if(user) {
+            this.users = this.users.filter((user) => user.id !== id);
+        }
+        return user;
+    }
+
+}
+
+module.exports = {Users};
