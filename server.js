cb21820c87d8884ccc7f279902113da9ee4baae4
AlanPintor <jaime.pintortejeda@georgebrown.ca>
2020-03-09 22:27:28 -0400

    styling pages

--- server.js
+++ server.js
@@ -1,31 +1,19 @@
 const express = require('express');
-const path = require('path');
-const app = express();
-
 //connect to DB
-var mongoonse = require('mongoose');
 const  connectDB = require('./DB/connection');
+const path = require('path');
+
+const app = express();
 //connect to DB
-connectDB()
+connectDB();
 
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 const port = process.env.PORT || 3000;
 const {generateMessage} = require('./utils/message.js');
 const {checkString} = require('./utils/checkString');
-const users = require('./DB/UserSchema');
-const msg = require('./DB/MessageSchema');
-
-
-
-// 8 Express Api - testing code - Alan
-// app.get('/api/history', function (req, res) {
-//     res.send('Chat History -  Returns a JSON list of chat here')
-//   })
-// app.post('/api/eventlog', function (req, res) {
-//     res.send('Chat History by Room Name - Return a JSON list of chat history for specific room name here')
-// })
-
+const {Users} = require('./utils/users');
+let users = new Users();
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'public'));
@@ -42,26 +30,9 @@ server.listen(port, () => {
     console.log(`Server is up on port ${port}`)
 });
 
-//MongoDB
-// Get users
-/*
-app.get('/users', function (req, res) {
-    console.log('getting all users');
-    users.find({}).exec(function (err, users) {
-        if (err) {
-            console.log(err)
-        } else {
-            console.log(res.json(users))
-        }
-    })
-})
-*/
-
-const guest = {};
+let messages = [];
 
 io.on('connection', (socket) => {
-
-
     console.log(`Socket connected: ${socket.id}`)
 
     socket.emit('previousMessages', messages)    
@@ -70,21 +41,10 @@ io.on('connection', (socket) => {
         if(!checkString(params.name) || !checkString(params.room)) {
             return callback("Name and room are required")
         }
-
-        guest[socket.id] = {name: params.name, room: params.room}
-
-        new users({
-            id = mongoonse.Types.ObjectId(),
-            userName: params.name,
-            room: params.room,
-            timeStamp: new Date().getTime(),
-            eventLog: String
-        })
-
         socket.join(params.room);
 
-        //users.removeUser(socket.id)
-        //users.addUser(socket.id, params.name, params.room);
+        users.removeUser(socket.id)
+        users.addUser(socket.id, params.name, params.room);
         
         io.to(params.room).emit('updateUsersList', users.getUserList(params.room));
 
@@ -96,6 +56,7 @@ io.on('connection', (socket) => {
     });
 
     socket.on('createMessage', (data) => {
+        //messages.push(data);
         let user = users.getUser(socket.id);
         if (user && checkString(data.message)) {
         io.to(user.room).emit('newMessage', generateMessage(user.name, data.message));
